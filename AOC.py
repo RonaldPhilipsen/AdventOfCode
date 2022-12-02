cadbde8b1fb1e7e32040ba2f423954763ae0b5ac
Ronald Philipsen <Github@rphilipsen.nl>
2022-12-02 09:43:58 +0100

    do day2

--- AOC.py
+++ AOC.py
@@ -13,6 +13,7 @@ import requests
 
 HTTP_OK = 200
 
+
 @cache
 def get_input(day: int) -> str:
     """gets the input for a given day of AOC
@@ -49,8 +50,7 @@ def get_input(day: int) -> str:
 
 
 def do_day_1():
-    """do the counting calories exercies
-    """
+    """do the counting calories exercise """
     input_str = get_input(1)
     input_list = input_str.split('\n')
     elf = []
@@ -68,3 +68,42 @@ def do_day_1():
 
     print(f'max_calories: {total_calories[0]}')
     print(f'top 3 calories: {sum(total_calories[0:3])}')
+
+
+def do_day_2():
+    """do the rock_paper scissors exercise """
+    I_ROCK = 'A'
+    I_PAPER = 'B'
+    I_SCISSORS = 'C'
+
+    O_ROCK_LOSE = 'X'
+    O_PAPER_DRAW = 'Y'
+    O_SCISSORS_WIN = 'Z'
+
+    SCORE_ROCK = 1
+    SCORE_PAPER = 2
+    SCORE_SCISSORS = 3
+
+    LOSS = 0
+    DRAW = 3
+    WIN = 6
+
+    scores_map = {
+        f'{I_ROCK} {O_ROCK_LOSE}': [(SCORE_ROCK + DRAW), (SCORE_SCISSORS + LOSS)],
+        f'{I_ROCK} {O_PAPER_DRAW}': [(SCORE_PAPER + WIN), (SCORE_ROCK + DRAW)],
+        f'{I_ROCK} {O_SCISSORS_WIN}': [(SCORE_SCISSORS + LOSS), (SCORE_PAPER + WIN)],
+        f'{I_PAPER} {O_ROCK_LOSE}': [(SCORE_ROCK + LOSS), (SCORE_ROCK + LOSS)],
+        f'{I_PAPER} {O_PAPER_DRAW}': [(SCORE_PAPER + DRAW), (SCORE_PAPER + DRAW)],
+        f'{I_PAPER} {O_SCISSORS_WIN}': [(SCORE_SCISSORS + WIN), (SCORE_SCISSORS + WIN)],
+        f'{I_SCISSORS} {O_ROCK_LOSE}': [(SCORE_ROCK + WIN), (SCORE_PAPER + LOSS)],
+        f'{I_SCISSORS} {O_PAPER_DRAW}': [(SCORE_PAPER + LOSS), (SCORE_SCISSORS + DRAW)],
+        f'{I_SCISSORS} {O_SCISSORS_WIN}': [(SCORE_SCISSORS + DRAW), (SCORE_ROCK + WIN)],
+    }
+
+    input_str = get_input(2)
+    input_list = input_str.split('\n')
+
+    scores_part1 = [scores_map[i][0] for i in input_list if i]
+    scores_part2 = [scores_map[i][1] for i in input_list if i]
+    print(f'Total: {sum(scores_part1)}')
+    print(f'Total: {sum(scores_part2)}')
