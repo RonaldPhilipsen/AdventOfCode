d6682268f3d66827c285709402ea951b00ebbac8
Ronald Philipsen <mail@rphilipsen.nl>
2023-12-02 09:20:01 +0100

    day1 2023

---
+++ 2023/day1.py
@@ -0,0 +1,73 @@
+import regex
+from typing import Dict, Union
+from advent_of_code import get_input
+
+
+input = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+
+numbers: Dict[str, str] = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4", 
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+
+input = get_input(2023, 1)
+
+
+def part1():
+    total_calibration_values = 0
+
+    for line in input.splitlines():    
+        if not line:
+            continue
+        digits = regex.findall(rf'(\d)', line, overlapped=True)
+        assert digits 
+
+        firstdigit = digits[0]
+
+
+        if len(digits) == 1:
+            lastdigit = firstdigit
+        else:
+            lastdigit = digits[-1]
+
+        calibrationval =  int(firstdigit + lastdigit)
+        total_calibration_values += calibrationval
+    print(total_calibration_values)
+
+def part2():
+    total_calibration_values = 0
+
+    for line in input.splitlines():    
+        if not line:
+            continue
+        digits = regex.findall(rf'(\d|{"|".join(numbers.keys())})', line, overlapped=True)
+        assert digits 
+
+        firstdigit = numbers[digits[0]] if digits[0] in numbers else digits[0]
+
+
+        if len(digits) == 1:
+            lastdigit = firstdigit
+        else:
+            lastdigit = numbers[digits[-1]] if digits[-1] in numbers else digits[-1]
+
+        calibrationval =  int(firstdigit + lastdigit)
+        total_calibration_values += calibrationval
+
+    print(total_calibration_values)
--- advent_of_code.py
+++ advent_of_code.py
@@ -1,11 +1,12 @@
+from pathlib import PosixPath
 import requests
 import os
 
-INPUT_PATH = os.getcwd() + '/input'
+INPUT_PATH = PosixPath(os.getcwd(),'input')
 HTTP_OK = 200
 
 
-def download_input(day: int) -> str:
+def download_input(year: int, day: int) -> str:
     """gets the input for a given day of AOC
 
     Args:
@@ -24,7 +25,7 @@ def download_input(day: int) -> str:
     if not cookie:
         raise Exception('failed to read cookie data')
 
-    url = f'https://adventofcode.com/2022/day/{day}/input'
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
     request = requests.Request('GET',  url, headers={
                                'Cookie': f'session={cookie}'})
     prepared_request = request.prepare()
@@ -34,22 +35,21 @@ def download_input(day: int) -> str:
 
     if result.status_code != HTTP_OK:
         raise Exception(
-            f'failed to get input for day {day}, status code {result.status_code}')
+            f'failed to get input for {year}/{day}, status code {result.status_code}')
 
     return result.text
 
 
-def get_input(day: int):
-    try:
-        os.stat(INPUT_PATH)
-    except FileNotFoundError:
-        os.mkdir(INPUT_PATH)
+def get_input(year: int, day: int):
+    yeardir = INPUT_PATH.joinpath(str(year))
+    yeardir.mkdir(parents=True, exist_ok=True)
 
-    if f'{day}_input.txt' in os.listdir(INPUT_PATH):
-        with open(f'{INPUT_PATH}/{day}_input.txt', 'r', encoding='UTF-8') as f:
+
+    if f'{day}_input.txt' in os.listdir(yeardir):
+        with open(f'{yeardir.as_posix()}/{day}_input.txt', 'r', encoding='UTF-8') as f:
             return f.read()
     else:
-        txt = download_input(day)
-        with open(f'{INPUT_PATH}/{day}_input.txt', 'w', encoding='UTF-8') as f:
+        txt = download_input(year=year, day=day)
+        with open(f'{yeardir.as_posix()}/{day}_input.txt', 'w', encoding='UTF-8') as f:
             f.write(txt)
         return txt
